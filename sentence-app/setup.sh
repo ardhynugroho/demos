9a52208a80cc14e9119b32c8fa23c6734db322a8
Ardhy Nugroho <116645464+ardhynugroho@users.noreply.github.com>
2023-07-12 20:24:36 +0700

    Update setup.sh
    update manifest file path

--- sentence-app/setup.sh
+++ sentence-app/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x
 git clone https://github.com/f5devcentral/sentence-demo-app.git
-cd sentence-demo-app
+cd sentence-demo-app/k8s-manifests
 
 kubectl create ns api
 kubectl create ns webapp-nginx
-kubectl apply -f k8s-manifests/sentence-api-all.yaml -n api
-kubectl apply -f k8s-manifests/sentence-nginx-webapp.yaml -n webapp-nginx
+kubectl apply -f sentence-api-all.yaml -n api
+kubectl apply -f sentence-nginx-webapp.yaml -n webapp-nginx
 
 kubectl apply -f ingress-vs-master.yaml
 kubectl apply -f ingress-vs-route-api.yaml -n api
